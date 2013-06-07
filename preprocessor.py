cdfa25222d578a6dc3bef42ff8a1019b8cf375db
Suman Jandhyala <sumanvyj@gmail.com>
2013-06-07 08:04:02 -0700

    Make preprocessor.py support generating features for multiple thresholds at once

--- preprocessor.py
+++ preprocessor.py
@@ -3,13 +3,13 @@ from __future__ import print_function
 import flesch_kincaid
 import sqlite3
 
-USEFUL_THRESHOLD = 2
+# USEFUL_THRESHOLD = 2
 
 def gen_feature_vec(review,
                     useful_bools,
                     useful_ratios,
                     biz_review_counts,
-                    threshold=USEFUL_THRESHOLD):
+                    threshold):
     text = review['text']
 
     useful = useful_bools[review['id']]
@@ -36,20 +36,6 @@ if __name__ == '__main__':
     db.row_factory = sqlite3.Row
     c = db.cursor()
 
-    useful_bools = c.execute('''SELECT r.id, r.useful >= ?
-                                FROM review AS r
-                              ''', (USEFUL_THRESHOLD,)).fetchall()
-    useful_bools = {k:v for k,v in useful_bools}
-
-    useful_ratios = c.execute('''SELECT
-                                   u.user_id,
-                                   1.0 * SUM(CASE WHEN r.useful >= ? THEN 1 ELSE 0 END) / COUNT(*)
-                                 FROM user AS u, review AS r
-                                 WHERE u.user_id = r.user_id
-                                 GROUP BY u.user_id
-                              ''', (USEFUL_THRESHOLD,)).fetchall()
-    useful_ratios = {k:v for k,v in useful_ratios}
-
     biz_review_counts = c.execute('''SELECT b.business_id, COUNT(*)
                                     FROM business AS b, review AS r
                                     WHERE b.business_id = r.business_id
@@ -58,9 +44,25 @@ if __name__ == '__main__':
     biz_review_counts = {k:v for k,v in biz_review_counts}
 
     reviews = c.execute('SELECT * FROM review').fetchall()
-    with open('features', 'w') as f:
-        for review in reviews:
-            feature = gen_feature_vec(review, useful_bools, useful_ratios, biz_review_counts)
-            print(','.join(feature), file=f)
+
+    for USEFUL_THRESHOLD in [1, 3, 5]:
+        useful_bools = c.execute('''SELECT r.id, r.useful >= ?
+                                    FROM review AS r
+                                  ''', (USEFUL_THRESHOLD,)).fetchall()
+        useful_bools = {k:v for k,v in useful_bools}
+
+        useful_ratios = c.execute('''SELECT
+                                       u.user_id,
+                                       1.0 * SUM(CASE WHEN r.useful >= ? THEN 1 ELSE 0 END) / COUNT(*)
+                                     FROM user AS u, review AS r
+                                     WHERE u.user_id = r.user_id
+                                     GROUP BY u.user_id
+                                  ''', (USEFUL_THRESHOLD,)).fetchall()
+        useful_ratios = {k:v for k,v in useful_ratios}
+
+        with open('features_' + str(USEFUL_THRESHOLD), 'w') as f:
+            for review in reviews:
+                feature = gen_feature_vec(review, useful_bools, useful_ratios, biz_review_counts, USEFUL_THRESHOLD)
+                print(','.join(feature), file=f)
 
     db.close()
